faf30ae1107238d4cc169da802555e1173cf7b43
darktype <darktype05@gmail.com>
2018-03-06 17:35:37 +0900

    Update simplestorage.sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -1,13 +1,23 @@
 pragma solidity ^0.4.0; 
- 
+
 contract SimpleStorage {
     uint storedData;
- 
+
     function set(uint x) {
         storedData = x;
     }
- 
-    function get() constant returns (uint) {
+
+    function get() constant returns (uint x) {
         return storedData;
+    }
+    
+    function getpassword(uint x) constant returns (string){
+        if(x==10){
+            return "Welcome to KOMSCO";
+        }
+        else{
+            return "bye";
+        }
+        
     }
 }
